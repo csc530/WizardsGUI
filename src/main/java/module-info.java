1f2ff121a559b230e8649ab86d5a30d3d8db961b
csc530 <cscousin@lakeheadu.ca>
2021-09-14 20:57:59 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module wizradcc.wizards {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens wizradcc.wizards to javafx.fxml;
+    exports wizradcc.wizards;
+}
---
+++ src/main/java/wizradcc/wizards/Controller.java
@@ -0,0 +1,59 @@
+package wizradcc.wizards;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtElement;
+
+    @FXML
+    private Spinner<Integer> spnPwrLvl;
+
+    @FXML
+    private RadioButton rbtnEvil;
+
+    @FXML
+    private RadioButton rbtnGood;
+
+    @FXML
+    private Separator seperator;
+
+    @FXML
+    private Label blurb;
+
+    @FXML
+    protected void create(){
+        String name = txtName.getText();
+        String element = txtElement.getText();
+        int power = spnPwrLvl.getValue();
+        char affinity = rbtnEvil.isSelected() ? 'E' : 'G';
+        Wizard wiz = new Wizard(name, element, power, affinity);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        spnPwrLvl.valueFactoryProperty().setValue(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 1999999999, 0));
+        ;
+        spnPwrLvl.getValueFactory().setWrapAround(true);
+        seperator.setVisible(false);
+    }
+}
